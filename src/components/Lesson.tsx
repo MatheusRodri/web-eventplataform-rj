92726f6ca28ffae9f274b68a75cfa623e05eb75c
Matheus <matheus.rj25@hotmail.com>
2022-07-21 14:43:29 -0300

    add vime and react-router-dom

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
     title: string;
@@ -18,12 +19,12 @@ export function Lesson(props: LessonProps) {
     })
 
     return (
-        <a href="#">
+        <Link to={`/event/lesson/${props.slug}`} className='group'>
             <span className="text-gray-300">
                 {availableDateFormat}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500" >
                 <header className="flex items-center justify-between">
                     {
                         isLessonAvailable ? (
@@ -41,7 +42,7 @@ export function Lesson(props: LessonProps) {
                             )
                     }
 
-                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border-1 border-green-300 font-bold">
+                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
                         {
                             props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'
                         }
@@ -53,6 +54,6 @@ export function Lesson(props: LessonProps) {
                     {props.title}
                 </strong>
             </div>
-        </a>
+        </Link>
     )
 }
